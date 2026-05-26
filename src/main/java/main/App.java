85f4d6baae1293c1826e681c1c362704b63b48c0
JoãoCarvalhoCarreiroNeto <joaonetocarvalho2101@gmail.com>
2026-05-25 21:59:48 -0300

    Mudando o Main para receber o que salvar no BD.

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,15 +2,24 @@ package main;
 
 import model.Produto;
 import service.EstoqueService;
-
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
         Produto p1 = new Produto();
-        p1.nome = "X-Tudo";
-        p1.preco = 25.0;
-        p1.quantidade = 10;
-        p1.categoria = "Sanduiche";
+
+        System.out.print("Nome: ");
+        p1.nome = scanner.nextLine();
+
+        System.out.print("Preço: ");
+        p1.preco = scanner.nextDouble();
+
+        System.out.print("Quantidade: ");
+        p1.quantidade = scanner.nextInt();
+
+        System.out.print("Categoria: ");
+        p1.categoria = scanner.nextLine();
 
         EstoqueService service = new EstoqueService();
 
